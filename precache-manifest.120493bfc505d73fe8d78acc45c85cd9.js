5b21b571aa1285507938b2a56d42a2e6fb118d83
Tarek Hassan <tarekhassan410@gmail.com>
2020-06-08 05:32:52 +0200

    'update'

---
+++ precache-manifest.120493bfc505d73fe8d78acc45c85cd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f12b86c5d61395849f",
+    "url": "/css/app.fb39a73e.css"
+  },
+  {
+    "revision": "5dd3408235ef61490c4bf2fdd6ddda0d",
+    "url": "/img/logo_transparent.5dd34082.png"
+  },
+  {
+    "revision": "be74c8bd906121aea3909380b65e7923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "422734dad8425f963929",
+    "url": "/js/about.a783acaa.js"
+  },
+  {
+    "revision": "89f12b86c5d61395849f",
+    "url": "/js/app.14355dd6.js"
+  },
+  {
+    "revision": "752e5b97924e60a2f97a",
+    "url": "/js/chunk-vendors.fb1241c2.js"
+  },
+  {
+    "revision": "dcd742a91a2f1cf48bee1f4b7413c964",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
